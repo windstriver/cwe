68b4829e444746da97959213563991cf97525ef7
windstriver <yongwang.ttu@gmail.com>
2017-10-02 16:12:24 -0500

    Add the case of flow around square cylinder with benchmark ercoftac ufr2-02.# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	new file:   0/B#	new file:   0/U#	new file:   0/k#	new file:   0/nuTilda#	new file:   0/nut#	new file:   0/p#	new file:   constant/transportProperties#	new file:   constant/turbulenceProperties#	new file:   generateMeshDict.pl#	new file:   postProcessing/forces/0.000000e+00/forceCoeffs.dat#	new file:   postProcessing/forces/0.000000e+00/post.py#	new file:   postProcessing/probes/0.000000e+00/p#	new file:   system/blockMeshDict#	new file:   system/controlDict#	new file:   system/decomposeParDict#	new file:   system/fvSchemes#	new file:   system/fvSolution#

---
+++ ufr2-02/postProcessing/forces/0.000000e+00/post.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+data = np.genfromtxt('forceCoeffs.dat', skip_header=9)
+time = data[:, 0]
+Cl = data[:, 3]
+
+# plt.plot(time, Cl)
+# plt.xlabel('Time')
+# plt.ylabel('Cl')
+# plt.show()
+
+N = 22829
+dt = np.sum(time[1:] - time[:N-1])/(N-1)
+fs = 1/dt
+
+f, Pxx_den = signal.welch(Cl, fs, nperseg=N)
+
+D = 0.04
+U = 0.54
+f_norm = f*D/U
+
+plt.plot(f_norm, Pxx_den)
+plt.xlabel('$\frac{n*D}{U}')
+plt.ylabel('SPD')
+plt.xlim([0, 0.02*f_norm[-1]])
+plt.show()
+
+St = f_norm[np.argmax(Pxx_den)]
+print('Strouhal value is: ', St)
