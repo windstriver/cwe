18b25148d52494a8985b324602972a730cd20e0c
Yong Wang <wan39502@quanah.hpcc.ttu.edu>
2018-01-05 14:19:46 -0600

    clean the repo

---
+++ lesTurbGen/lesTurbGen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N LES_INLET
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -pe mpi 36
+#$ -P quanah
+
+cd testCase
+
+#source $HOME/OpenFOAM/OpenFOAM-5.0/etc/bashrc
+
+blockMesh
+
+../bin/createHDF5
+
+../bin/writeHDF5
+
+decomposePar
+
+#module load gnu openmpi
+
+mpirun -np $NSLOTS pimpleFoam -parallel
+
+reconstructPar
+
