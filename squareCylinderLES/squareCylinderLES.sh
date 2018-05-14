a80a3fc9aa5627eb91e318a52787007e2bcbb28f
Yong Wang <wan39502@quanah.hpcc.ttu.edu>
2018-05-14 12:32:46 -0500

    Add project of LES of flow past square cylinder

---
+++ squareCylinderLES/squareCylinderLES.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N SC_LES
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -pe mpi 72
+#$ -P quanah
+
+cd testCase
+
+#source $HOME/OpenFOAM/OpenFOAM-5.0/etc/bashrc
+
+#blockMesh
+
+#checkMesh
+
+#../bin/createHDF5
+
+#../bin/writeHDF5
+
+#decomposePar
+
+#module load gnu openmpi
+
+#mpirun -np $NSLOTS pimpleFoam -parallel
+
+#reconstructPar
+
+foamToEnsight -fields '("p.*" "U.*")'
+
