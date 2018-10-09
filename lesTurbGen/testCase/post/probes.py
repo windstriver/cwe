da8b5342ace9fad3938c4040c91b7b134e4e091a
windstriver <yongwang.ttu@gmail.com>
2018-10-09 16:14:22 -0500

    Clean the repo

--- lesTurbGen/testCase/post/probes.py
+++
@@ -1,9 +0,0 @@
-# Convert probe post data to csv
-
-probePost = "../postProcessing/probes/0/U"
-probeOut = "internal_cell_velocity.csv"
-
-with open(probePost, 'r') as fin:
-    with open(probeOut, 'w') as fout:
-        for line in fin:
-            fout.write(line.replace('(', ' ').replace(')', ' '))
