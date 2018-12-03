a9d96ab93e35602a9a2d754c02cef928d00d58ab
windstriver <yongwang.ttu@gmail.com>
2018-12-03 11:52:03 -0600

    Clean the repo

--- tallBuildingLES/postPy/postPyTest.py
+++ tallBuildingLES/postPy/postPyTest.py
@@ -3,11 +3,12 @@ import postPy
 boundaryCloudDir = '/lustre/work/wan39502/' + 'tallBuildingLES/testCase/' + \
                    'postProcessing/boundaryCloudPresBuildingExp/'
 
-fvPatchSfFile = './buildingPatchFaceAreaVectors'
+fvPatchSfFile = './buildingPatchFaceAreaVectorsExp'
 
 probePositionFile = '/lustre/work/wan39502/' + \
                     'tallBuildingLES/ExpTPU/' + \
                     'Location_of_CFD_pressure_probes.csv'
+
 presCoefExpFile = '/lustre/work/wan39502/' + \
                   'tallBuildingLES/ExpTPU/' + \
                   'presCoefExp.csv'
@@ -15,12 +16,15 @@ presCoefExpFile = '/lustre/work/wan39502/' + \
 # internalCloudDir = '/lustre/work/wan39502/' + 'tallBuildingLES/testCase/' + \
 #                    'postProcessing/internalCloudUUpstream/'
 
-# bc = postPy.BoundaryCloudP(boundaryCloudDir = boundaryCloudDir, \
-#                            fvPatchSfFile = fvPatchSfFile)
+bc = postPy.BoundaryCloudP(boundaryCloudDir = boundaryCloudDir, \
+                           fvPatchSfFile = fvPatchSfFile)
+
 bcExp = postPy.BoundaryCloudPExp(probePositionFile=probePositionFile, \
                                  presCoefExpFile=presCoefExpFile, \
                                  fvPatchSfFile=fvPatchSfFile)
-# baseCoefDF = bc.baseForceMomentCoef()
+
+baseCoefDF = bc.baseForceMomentCoef()
+
 baseCoefExpDF = bcExp.baseForceMomentCoef()
 
 # ic = postPy.InternalCloudU(Uh, H, B, D, deltaT, internalCloudDir)
